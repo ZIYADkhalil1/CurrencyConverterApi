d963a9d42e43078a4007e4c633f065a223d9651c
Zyad Ibrahim KhaliL <zyadmohamed94@gmail.com>
2024-12-06 03:00:00 +0200

    adding currency exchange api

---
+++ currencyExchange/src/main/java/com/example/CEA/Controller.java
@@ -0,0 +1,28 @@
+package com.example.CEA;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RequiredArgsConstructor
+@RestController
+public class Controller {
+	
+	private final CurrencyExchangeService currencyExchangeService;
+	
+	@GetMapping("/convert")
+	public Double getMethodName(@RequestParam Double amount,@RequestParam String curCode) throws StreamReadException, DatabindException, IOException {
+		return currencyExchangeService.convertCurrency(amount, curCode);
+	}
+	
+
+}
